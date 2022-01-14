60b00ce6ba36127457d876a7b55eed61b5eb44da
Adumule27 <ese.aduniya.mulugeta@gmail.com>
2022-01-15 00:45:17 +0300

    The Assignment

---
+++ secondLargest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace find_the_second_largest_number
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int larger = 0;
+            int second = 0;
+            int[] numbers = {
+                  12,20,30,40,38
+            };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+
+
+                if (numbers[i] > larger)
+                {
+                    second = larger;
+                    larger = numbers[i];
+                }
+                else if (numbers[i] > second)
+                {
+                    second = numbers[i];
+                }
+
+            }
+            System.Console.WriteLine(second);
+
+
+        }
+
+
+    }
+}
